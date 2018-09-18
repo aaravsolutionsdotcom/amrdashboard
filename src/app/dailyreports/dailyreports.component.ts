f54f74f6d38f21db1001162b79faf363f6f18e2c
prudhviakella <prudhvi.prudhvi.akella@gmail.com>
2018-09-18 14:51:21 +0530

    Added date validation to reports page

--- src/app/dailyreports/dailyreports.component.ts
+++ src/app/dailyreports/dailyreports.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'; 
+import { Validators, FormGroup, FormControl, ValidationErrors } from "@angular/forms";
 
 @Component({
   selector: 'app-dailyreports',
@@ -10,6 +11,14 @@ export class DailyreportsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+      var date = new Date();
+      var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+      var lastDay = new Date();
+      console.log('lastdate and firstday' + firstDay + lastDay)
   }
 
+    fromdate = new FormControl(new Date());
+    todate = new FormControl(new Date());
+    maxDate = new Date()
+    minDate = new Date(this.maxDate.getFullYear(), this.maxDate.getMonth(), 1); 
 }
--- src/app/monthlyreports/monthlyreports.component.ts
+++ src/app/monthlyreports/monthlyreports.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormGroup, FormControl, ValidationErrors } from "@angular/forms";
 
 @Component({
   selector: 'app-monthlyreports',
@@ -8,8 +9,14 @@ import { Component, OnInit } from '@angular/core';
 export class MonthlyreportsComponent implements OnInit {
 
   constructor() { }
-
-  ngOnInit() {
-  }
-
+    firstDayofyear;
+       
+    ngOnInit() {
+        var date = new Date();
+        this.firstDayofyear = new Date(date.getFullYear(), 1);       
+    }
+    fromdate = new FormControl(new Date());
+    todate = new FormControl(new Date());
+    maxDate = new Date()
+    minDate = new Date(this.maxDate.getFullYear(), 0); 
 }
